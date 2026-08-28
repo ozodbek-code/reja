6ccf938c08b133478d28ffd8f8078d397b41b17c
Ozodbek <ozodbekamirov1188@gmail.com>
2026-08-28 18:23:51 +0900

    fix folders

--- server.js
+++ server.js
@@ -11,16 +11,29 @@ app.use(express.urlencoded({extended: true}));
 // 2 : Session code
 
 // 3 Views code
-app.set("views", "views");
+app.set("view", "view");
 app.set("view engine", "ejs");
 
 
 // 4 Routing code
-app.get("/hello", function(req, res){
-    res.end("<h1>Hello World <h1>");
+// 4 Routing code
+
+// 4 Routing code
+
+app.get("/", function(req, res) {
+    res.send("<h1></h1>");
 });
-app.get("/gift", function(req, res){
-    res.end("<h1>siz sovgalar bolimidasz <h1>");
+
+app.get("/hello", function(req, res) {
+    res.end("<h1>Hello World</h1>");
+});
+
+app.get("/gift", function(req, res) {
+    res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
+});
+
+app.get("/author", function(req, res) {
+    res.render("author");
 });
 
 
