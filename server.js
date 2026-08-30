82ece20bf7f5c95e7aa565b187a5ce6231027ca6
Ozodbek <ozodbekamirov1188@gmail.com>
2026-08-30 16:36:47 +0900

    new changes

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+    
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -11,17 +22,17 @@ app.use(express.urlencoded({extended: true}));
 // 2 : Session code
 
 // 3 Views code
-app.set("view", "view");
+app.set("views", "view");
 app.set("view engine", "ejs");
 
 
 // 4 Routing code
-// 4 Routing code
-
-// 4 Routing code
-
+app.get("/author", function(req, res) {
+    res.render("author", {user: user} );
+});
 app.get("/", function(req, res) {
-    res.send("<h1></h1>");
+    res.render("author", {user: user});
+    // yoki: res.redirect("/author");
 });
 
 app.get("/hello", function(req, res) {
@@ -30,10 +41,6 @@ app.get("/hello", function(req, res) {
 
 app.get("/gift", function(req, res) {
     res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
-});
-
-app.get("/author", function(req, res) {
-    res.render("author");
 });
 
 
