5eb4d8be656b88547d4370cebc5a2da7ba4343a2
Ozodbek <ozodbekamirov1188@gmail.com>
2026-08-30 16:55:59 +0900

    feat : asynchronous function

--- train.js
+++ train.js
@@ -9,24 +9,39 @@ const list =  [
   "Hayotdan zavqlan", // 60+ 
 ];
 
-function maslaxatBering(a, callback) {
-    if(typeof a !== 'number') callback("insert a number", null);
-    else if(a <= 20) callback(null, list[0]);
-    else if(a > 20 && a <= 30) callback(null, list[1]);
-    else if(a > 30 && a <= 40) callback(null, list[2]);
-    else if(a > 40 && a <= 50) callback(null, list[3]);
-    else if(a > 50 && a <= 60) callback(null, list[4]);
+async function maslaxatBering(a) {
+    if(typeof a !== 'number') throw new ERROR("insert a number");
+    else if(a <= 20) return list [0];
+    else if(a > 20 && a <= 30) return list[1];
+    else if(a > 30 && a <= 40) return list[2];
+    else if(a > 40 && a <= 50) return list[3];
+    else if(a > 50 && a <= 60) return list[4];
     else {
-        setTimeout(function() {
-         callback(null, list[5]);   
-        }, 5000);
-    } 
-}
-console.log("passed here o");
-maslaxatBering(10, (err, data) => {
-    if(err) console.log('ERROR:', err);
-    else {
-    console.log("javob:", data);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+            }, 5000);
+        });
+        } 
     }
-});
-console.log("passed here 1");
+
+// then/ catch
+//console.log("passed here o");
+//maslaxatBering(20)
+//.then((data) => {
+ //console.log('javob:', data);
+//});
+//.catch((err) => {
+  //console.log("ERROR:", err);
+//});
+//console.log("passed here 1");
+
+// async/await
+async function run() {
+   let javob = await maslaxatBering(20);
+   console.log(javob);
+   javob = await maslaxatBering(31);
+   console.log(javob);
+   javob = await maslaxatBering(41);
+   console.log(javob);
+}
+run();
