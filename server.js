7b040b34693af2f06b64bffec7e7520ba29fc352
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-02 18:50:10 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -1,51 +1,45 @@
-console.log("Web Serverni boshlash");
+console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
 let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
+fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
-        console.log("ERROR:", err);
+        console.log("ERROR", err);
     } else {
-        user = JSON.parse(data)
+        user = JSON.parse(data);
     }
-    
-});
+})
 
-// 1 Kirish code
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// 2 : Session code
+// 2 sessions code
 
 // 3 Views code
-app.set("views", "view");
+app.set("views", "./view");
 app.set("view engine", "ejs");
 
-
-// 4 Routing code
-app.get("/author", function(req, res) {
-    res.render("author", {user: user} );
+// 4 Routing Code
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
 });
-app.get("/", function(req, res) {
+
+app.get("/author", (req, res) => {
     res.render("author", {user: user});
-    // yoki: res.redirect("/author");
-});
+})
 
-app.get("/hello", function(req, res) {
-    res.end("<h1>Hello World</h1>");
+app.get("/", function(req, res) {
+    res.render("reja");
 });
-
-app.get("/gift", function(req, res) {
-    res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
-});
-
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function (){
-    console.log(`The server is running successfully on port: ${PORT}`);
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on port: ${PORT},http://localhost:${PORT}`);
 });
